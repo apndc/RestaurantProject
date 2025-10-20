47b33bc39d7ae9185baded7a34fe08697c3eca02
Bobby <bobbymcdonald422@gmail.com>
2025-10-20 17:23:25 -0400

    Fixed Formatting V2

--- RestaurantProject/db/Schema/RestaurantInfo.py
+++ RestaurantProject/db/Schema/RestaurantInfo.py
@@ -13,9 +13,11 @@ class RestaurantInfo(Base):
     LocationID = Column(Integer, ForeignKey('Location.LocationID'), nullable=False)
 
     #Other info in table 
-
     PhoneNumber = Column(String(10))
     Cusine = Column(String(40))
     Capacity = Column(Integer)
-    
+    Fee = Column(Integer)
 
+    def __repr__(self):
+        return f""" Restuarnt ID: {self.RID}, User ID: {self.UserID}, Locaction ID: {self.LocationID}, Phone Number: {self.PhoneNumber}, 
+        Cusine: {self.Cusine}, Capacity: {self.Capacity}, Fee: {self.Fee}"""
