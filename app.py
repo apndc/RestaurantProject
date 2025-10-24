b1a2496714ace849894fa1289d13a271c61af934
Alicia G <jwgirl1200@hotmail.com>
2025-10-23 23:58:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 """app.py: render and route to webpages"""
-from flask import render_template
+from flask import Flask, render_template
 
-from db.server import app
+app = Flask(__name__)
 
-# create a webpage based off of the html in templates/index.html
 @app.route('/')
-def index():
-    return render_template("index.html")
+def eventpage():
+    return render_template('bookit-eventpage.html')
 
-if __name__ == "__main__":
-    # debug refreshes your application with your new changes every time you save
+if __name__ == '__main__':
     app.run(debug=True)
