c3c773ad24ae820aff3ac74a981c02f232174d8a
Alicia G <jwgirl1200@hotmail.com>
2025-11-10 01:15:50 -0500

    Forgot to take out the email column

--- RestaurantProject/db/schema/EP_Verification.py
+++ RestaurantProject/db/schema/EP_Verification.py
@@ -5,5 +5,4 @@ class EP_Verification(Base):
     __tablename__ = 'ep_verification'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
     verification_code = Column(String, nullable=False)
--- RestaurantProject/db/schema/RO_Verification.py
+++ RestaurantProject/db/schema/RO_Verification.py
@@ -5,5 +5,4 @@ class RO_Verification(Base):
     __tablename__ = 'ro_verification'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
     verification_code = Column(String, nullable=False)
